e3b38683824be1ff86a17524e679106e7224e2e0
0xbeny <behnammk125@gmail.com>
2023-10-12 13:24:32 +0400

    added header text to committess page

--- src/pages/CommitteesPage.tsx
+++ src/pages/CommitteesPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text, Heading } from "@chakra-ui/layout";
+import { Box, Flex, Text, Heading, Container } from "@chakra-ui/layout";
 import React from "react";
 import { styled } from "styled-components";
 import { v4 as uuid } from "uuid";
@@ -30,16 +30,26 @@ const CommitteesList = [
 ];
 const CommitteesPage = () => {
   return (
-    <Box className="grid grid-cols-12 grid-rows-12">
-      {CommitteesList.map(({ id, name, path, description }) => (
-        <Link to={path} className="col-span-12 row-span-1 sm:col-span-4">
-          <BoxWrapper key={id} className="m-2">
-            <Heading size={"md"}>{name}</Heading>
-            <Text as={"p"}>{description}</Text>
-          </BoxWrapper>
-        </Link>
-      ))}
-    </Box>
+    <Container maxW={"container.xl"} className="py-4">
+      <Box className="text-start">
+        <Heading size={"lg"}>Committees Management</Heading>
+        <Text>
+          Here you can find all the 3 different groups of people, that are
+          taking care of evaluating and voting on proposals
+        </Text>
+      </Box>
+      <Box className="m-4"></Box>
+      <Box className="grid grid-cols-12 grid-rows-12">
+        {CommitteesList.map(({ id, name, path, description }) => (
+          <Link to={path} className="col-span-12 row-span-1 sm:col-span-4">
+            <BoxWrapper key={id} className="m-2">
+              <Heading size={"md"}>{name}</Heading>
+              <Text as={"p"}>{description}</Text>
+            </BoxWrapper>
+          </Link>
+        ))}
+      </Box>
+    </Container>
   );
 };
 
